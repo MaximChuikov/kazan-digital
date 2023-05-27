6985eb8193f07474bae1c1435285019c20cf506c
Seraphic <danilsocol-2002@mail.ru>
2023-05-27 12:31:46 +0500

    backend start

---
+++ src/backend/models/TypesPoint.ts
@@ -0,0 +1,12 @@
+enum TypesPoint {
+    Ramp = 0,
+    Elevator = 1,
+    TrafficLightSignal = 2,
+    Toilet=3,
+    ComfortableBusStation,
+    UnComfortableBusStation,
+    Obstacle
+
+}
+
+export default TypesPoint
