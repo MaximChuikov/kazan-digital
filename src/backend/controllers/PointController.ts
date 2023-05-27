ae1cbbb97dd01446782c509a4080ecb8f296edab
Seraphic <danilsocol-2002@mail.ru>
2023-05-28 00:33:22 +0500

    create evaluation

--- src/backend/controllers/PointController.ts
+++ src/backend/controllers/PointController.ts
@@ -16,12 +16,21 @@ class PointController {
     deletePoint(coordinate : string) {
         const point = PointRepository.findPointByCoordinate(coordinate)
 
-        if(point.countVotesToDelete <= 5){
+        if(point.countVotesToDelete!! >= 5){
             return PointRepository.deleteByCoordinate(coordinate)
         }
-        point.countVotesToDelete++
+        point.countVotesToDelete!!++
         PointRepository.savePoint(point)
     }
+
+    calculateRating(point : Point) : Point {
+        console.log(point)
+        console.log(point.evaluations)
+        point.rating =  point.evaluations.map(e => e.score).reduce((pV, cur) => pV + cur) / point.evaluations.length
+        return  PointRepository.savePoint(point)
+    }
+
+    //addVoteCreate
 }
 
 export default new PointController()
