2446af87052cbb8fae005d2390ea33d25ca032c6
Maxim <machsim26@gmail.com>
2023-05-27 18:53:32 +0500

    стили для точек

--- src/components/Points/SimplePoint.tsx
+++ src/components/Points/SimplePoint.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Point from "../../backend/models/Point"
-import {Circle, Marker, Tooltip} from "react-leaflet";
-import {Icon} from "leaflet";
-import {convertToTooltipDescription, convertToUrl} from "../../backend/utils/enumTo";
+import {Circle, ImageOverlay, Marker, Tooltip} from "react-leaflet";
+import {Icon, LatLngBounds, LatLngBoundsExpression, LatLngTuple} from "leaflet";
+import {convertToUrl, convertToTooltipDescription} from "../../backend/utils/enumTo";
+import styles from "./point.module.scss"
 
 const SimplePoint = (point: Point) => {
     return (
-        <Marker position={[point.x, point.y]} shadowPane="green" icon={new Icon({
+        <Marker position={[point.x, point.y]} icon={new Icon({
             iconUrl: convertToUrl(point.type),
-            iconSize: [30, 30]
-        })}>
-            <Circle opacity={0.2} center={[point.x, point.y]} radius={400}/>
+            iconSize: [30, 30],
+            // shadowUrl: "/marker-icon-2x.png",
+            // shadowSize: [40, 40],
+            className: styles.point
+        })} >
             <Tooltip>
-                {point.description || convertToTooltipDescription(point.type) || "Не указано"}
+                {point.description || convnrtToTooltipDescription(point.type) || "Не указано"}
             </Tooltip>
         </Marker>
     );
